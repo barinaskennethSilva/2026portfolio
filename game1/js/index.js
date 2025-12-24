65ee12572f373adf64eb39d951b136a74fb3a849
Kenneth Barinas <barinaskennethSilva>
2025-12-24 19:07:37 +0800

    3 commit

--- game1/js/index.js
+++ game1/js/index.js
@@ -1,16 +1,43 @@
 window.addEventListener('load', () => {
-  
+  const player = document.getElementById('player');
+  const joystick = document.getElementById('joystick');
+  const stick = document.getElementById('stick');
 
-const player = document.getElementById('player');
-const btnleft = document.getElementById('btnleft');
+  let playerPos = 45;
+  let dragging = false;
 
-let playerPos = 45;
+  const center = 60; // joystick center
+  const maxDistance = 40;
+  const maxSpeed = 1.2;
+  joystick.addEventListener('touchstart', () => {
+    dragging = true;
+  });
 
-btnleft.addEventListener('touchstart', ()=>movePlayer(-5));
+  joystick.addEventListener('touchmove', (e) => {
+    if (!dragging) return;
 
-function movePlayer(amount){
- /* if (gameOver) return;*/
-  playerPos = Math.min(90, Math.max(0,playerPos + amount));
-  player.style.left = playerPos  + "%"
-}
+    const touch = e.touches[0];
+    const rect = joystick.getBoundingClientRect();
+
+    let x = touch.clientX - rect.left - center;
+    x = Math.max(-maxDistance, Math.min(maxDistance, x));
+
+    // move stick
+    stick.style.transform = `translateX(${x}px)`;
+//speed calculation
+const distance = Math.abs(x);
+const speed = (distance / maxDistance) * maxSpeed;
+const direction = Math.sign(x);
+playerPos += direction * speed;
+
+    // move player
+    playerPos += x * 0.02;
+    playerPos = Math.min(90, Math.max(0, playerPos));
+    player.style.left = playerPos + "%";
+  });
+
+  joystick.addEventListener('touchend', () => {
+    dragging = false;
+    stick.style.transform = "translateX(0)";
+  });
 });
