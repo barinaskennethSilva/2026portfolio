b254dcb88d2e289dd5ecc459770789dec03e9d81
Kenneth Barinas <barinaskennethSilva>
2026-01-23 15:10:39 +0800

    Jan 23 2026

--- Python/Learning.py
+++ Python/Learning.py
@@ -12,7 +12,7 @@ GRAY = (180,180,180)
 BLACK = (0, 0, 0)
 
 #box hero
-y = 200
+y = 500
 x = 200
 width = 100
 height = 100
@@ -21,9 +21,11 @@ vel = 20
 bullets = []
 bullet_speed = 10
 #button
-left_btn = pygame.Rect(50, 430, 100, 80)
-right_btn = pygame.Rect(250, 430, 100, 80)
-fire_btn = pygame.Rect(450,430,100,80)
+up_btn = pygame.Rect(150, 730, 100, 80)
+left_btn = pygame.Rect(50, 830, 100, 80)
+right_btn = pygame.Rect(250, 830, 100, 80)
+down_btn = pygame.Rect(150,1000,100,80)
+fire_btn = pygame.Rect(450,730,100,80)
 
 font = pygame.font.SysFont(None, 30)
 
@@ -36,13 +38,18 @@ while run:
     pygame.draw.rect(win, BLUE, (x, y, width, height))
 
     # Draw buttons
+    pygame.draw.rect(win,GRAY, up_btn)
     pygame.draw.rect(win, GRAY, left_btn)
     pygame.draw.rect(win, GRAY, right_btn)
     pygame.draw.rect(win, GRAY, fire_btn)
+    pygame.draw.rect(win,GRAY,down_btn)
 
-    win.blit(font.render("LEFT", True, BLACK),(75, 465))
-    win.blit(font.render("RIGHT", True, BLACK),(265, 465))
-    win.blit(font.render("Fire",True, BLACK),( 480,465))
+    win.blit(font.render("UP", True, BLACK),(185,765))
+    win.blit(font.render("LEFT", True, BLACK),(75, 895))
+    win.blit(font.render("RIGHT", True, BLACK),(265, 895))
+    win.blit(font.render("DOWN",True,BLACK),(170,1030))
+    
+    win.blit(font.render("Fire",True, BLACK),( 480,765))
     for event in pygame.event.get():
           if event.type == pygame.QUIT:
               run = False
@@ -52,9 +59,15 @@ while run:
                    x-=vel
                if right_btn.collidepoint(event.pos) and x < WIDTH - width:
                    x+=vel
-               if fire_btn.collidepoint(event.pos) and pygame.Rect(x + width // 2 - 2, y, 4, 10):
+               if fire_btn.collidepoint(event.pos):
+                  bullet = pygame.Rect(x + width // 2 - 2, y, 4, 10)
                   bullets.append(bullet)
     
+    for bullet in bullets[:]:
+              pygame.draw.rect(win, BLACK, bullet)   
+              bullet.y -=  bullet_speed
+              if bullet.y < 0:
+                  bullets.remove(bullet)
     
     pygame.display.update()
 
