37a2a4f447734be6e13bc739da0e94282b6338fa
Kenneth Barinas <98674347+barinaskennethSilva@users.noreply.github.com>
2026-07-07 14:36:05 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,10 +10,10 @@ window.mySkillsFunc = function(){
 let project =[{"img":"img/ChicksGPT.jpg","name":"ChicksGPT","techStock":"html,Django","intro":"ChicksGPT is a prototype artificial intelligence system developed using a Transformer architecture.","link":"https://github.com/barinaskennethSilva/ADA_WONG_AI"},{"img":"img/pw1.jpg","name":"ChatWeb","techStock":"Vuejs,PHP","intro":"A chatbox website that allow user to send message in realtime."},{"img":"img/hunt.jpg","name":"Animal Hunting","techStock":"Python","intro":"Start hunt animals for coins."},{"img":"img/pokeMo.jpg","name":"pokeMo","techStock":"Python","intro":"Come and catch pokemon.","link":"https://github.com/barinaskennethSilva/Pokemo"},{"img":"img/DataAnalyst.jpg","name":"Analysis Data Company Expenses","techStock":"Excel","intro":"This project tracks and analyzes company expense data to uncover spending patterns and highlight areas for cost reduction.","link":"#"}]
 const output = document.getElementById("output");
 project.forEach(function(project){
-output.innerHTML += `  <div class="game-card">      <img src='${project.img.trim()}'alt='${project.img}'><h3>${project.name}</h3><p>${project.intro}</p><span>${project.techStock}</span>      <div class="likeView"><button class="btnLike" data-btnprog_name='${project.name}'><i class="fa fa-thumbs-up"></i><p class="likeVal" id="like-${project.name}">0</p></button><button ><i class="fa fa-eye"></i><p id="numbView">0</p></button></div>
+output.innerHTML += `  <div class="game-card">      <img src='${project.img.trim()}'alt='${project.img}'><h3>${project.name}</h3><p>${project.intro}</p><span>${project.techStock}</span>      <div class="likeView"><button class="btnLike" data-btnprog_name='${project.name}'><i class="fa fa-thumbs-up"></i><p class="likeVal" id="like-${project.name}">0</p></button><button class="btnLike" data-btnprog_name='${project.name}'><i class="fa fa-comments-o"></i><p class="likeVal" id="like-${project.name}">0</p></button><button ><i class="fa fa-eye"></i><p id="numbView">0</p></button></div>
       <a href='${project.link}' class="play-btn">View Documentation</a></div>`;  
 });
-
+	
 
 const btnLike = document.querySelectorAll(".btnLike");
  
@@ -32,4 +32,4 @@ btnLike.forEach(button => {
     
   });
 });
-    
+  
